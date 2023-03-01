dc37b968ed4138bfc2903faa1c82c20d2ac9b096
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2023-03-01 11:08:40 -0800

    added pointers

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for positive or negative
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i)
+}

9f4ab20c87b8eb5b50998955b7b797a89e7683f2
sawacha emmanuel <sawachaemmanuel@gmail.com>
2023-08-24 17:22:50 -0700

    list len check

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,16 +5,16 @@
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
- * Return: number of elements in s
+ * Return: number of elements in h
  */
-size_t list_len(const list_t *s)
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
 	while (h)
 	{
 		n++;
-		s = s->next;
+		h = h->next;
 	}
 	return (n);
 }
